9ce8ed6d5b2b084138cb7c131361f8d2d138cc81
Martin Cooper <martin.cooper@trant.co.uk>
2012-11-22 15:21:41 +0000

    Created a rails and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trantbook::Application.config.secret_token = '3b8e64cbd3f8e1b78b50208ad60e26c86aa8b9accf95518b7af9385d68b836a1b58ba2c4a55287d1bbd8252db1dd6988df8ce6ec9382e2464fb0b675e72bbfa7'
