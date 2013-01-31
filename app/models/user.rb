b4da008c1ed95f84e1667c0efa05ee0e083cb613
Martin Cooper <martin.cooper@trant.co.uk>
2013-01-31 12:50:30 +0000

    Updated the frontend for the new authentication system. Added simple form, updated several forms in simple form, added log in on/out on navbar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,18 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+  
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+  
+  validates :profile_name, presence: true
+  
   # attr_accessible :title, :body
+
+  has_many :statuses
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
